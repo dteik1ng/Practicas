07b9438dafa096ab7bf62b27d5614def81e1cb09
Luis <posadaluis451@gmail.com>
2023-10-04 18:27:15 -0500

    Segunda Parte

--- Login/src/controllers/auth.controller.js
+++ Login/src/controllers/auth.controller.js
@@ -28,4 +28,43 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => res.send("login");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const userFound = await User.findOne({ email });
+    if (!userFound) return res.status(404).json({ message: "User not Fount" });
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+    if (!isMatch)
+    return res.status(400).json({ message: "Error en Credentials" });
+  
+    const token = await createTokenAccess({ id: userFound._id });
+    res.cookie('token', token);
+    res.status(201).json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+    });
+  } catch (error) {
+    req.status(500).json({ message: error.message });
+  }
+};
+
+export const logout = (req, res) => {
+  res.cookie('token', '', {
+    expires : new Date(0),
+  });
+  return res.status(200);
+};
+
+export const profile = (req, res) => {
+  
+  const userFound = User.findById(req.user.id);
+  if(!userFound) return res.status(400).json({ message : "User not Found" });
+
+  res.status(201).json({
+    id: userFound._id,
+    username: userFound.username,
+    email: userFound.email
+  });
+}
