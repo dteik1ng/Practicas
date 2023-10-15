afe1f527d201ea986d483c19ef2a9992d67571df
Luis <posadaluis451@gmail.com>
2023-10-15 13:20:42 -0500

    Quinta parte

--- Login/client/src/pages/RegisterPage.jsx
+++ Login/client/src/pages/RegisterPage.jsx
@@ -1,23 +1,51 @@
 import { useForm } from 'react-hook-form';
-import { registerRequest } from '../api/auth';
+import { useAuth } from '../context/AuthContext';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function RegisterPage() {
 
-    const { register, handleSubmit} = useForm();
+    const { register, handleSubmit, formState : {errors} } = useForm();
+    const { signup, isAuthenticated, errors: ResgisterErrors } = useAuth();
+    const navigate = useNavigate();
+    
 
+    useEffect(() => {
+      if (isAuthenticated) navigate('/tasks');
+    }, [isAuthenticated, navigate])
+
+    const onSubmited = handleSubmit(async (values) => {
+      signup(values); //console.log(values);
+    })
   return (
     <div className='bg-zinc-800 max-w-md p-10 rounded-md'>
-        <form onSubmit={handleSubmit( async (values) => {
-            console.log(values);
-            const res = await registerRequest(values)
-            console.log(res);
-        })}>
+
+      {
+        ResgisterErrors.map((error, i) => (
+          <div className='bg-red-500 text-white p-2' key={i}>
+            {error}
+          </div>
+        ))
+      }
+        <form onSubmit={onSubmited}>
             <input type="text" {...register("username", {required:true})} placeholder='username'
             className='w-full bg-zinc-600 text-white px-4 py-4 rounded-md my-2' />
+            {
+              errors.username && <p className='text-red-500'>Username is Required</p>
+            }
+
             <input type="email" {...register("email", {required:true})} placeholder='email'
             className='w-full bg-zinc-600 text-white px-4 py-4 rounded-md my-2' />
+            {
+              errors.email && <p className='text-red-500'>Email is Required</p>
+            }
+
             <input type="password" {...register("password", {required:true})} placeholder='password'
             className='w-full bg-zinc-600 text-white px-4 py-4 rounded-md my-2' />
+            {
+              errors.password && <p className='text-red-500'>Password is Required</p>
+            }
+
             <button type="submit" className="bg-blue-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded">Register</button>
         </form>
     </div>
--- Login/src/index.js
+++ Login/src/index.js
@@ -1,6 +1,5 @@
-import app from './app.js';
-import {connectDB} from './db.js';
-
+import app from "./app.js";
+import { connectDB } from "./db.js";
 
 connectDB();
-app.listen(5000, () => console.log('Server On PORT 5000'));
+app.listen(5000, () => console.log("Server On PORT 5000"));
